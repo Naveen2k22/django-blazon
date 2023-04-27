dd776a3863fa48b8984dedfc1d3cc12ade7eb9af
naveen2k22 <naveen2001010601@gmail.com>
2023-04-27 13:12:06 +0530

    modify admin page header using base html

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Vendor
+from .models import Vendor, User
 
 admin.site.register(Vendor)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["f_name", "l_name", "email", "address"]
+    search_fields = ["f_name"]
+
+admin.site.register(User, UserAdmin)
